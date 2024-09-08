6c4a22d336b413b1a1f34c9d9efe73c8482d1f35
HamzaAcb <hamzaachoubir98@gmail.com>
2024-09-08 13:41:53 -0400

    Creating initial layout

---
+++ podia/src/components/common/Layout.jsx
@@ -0,0 +1,17 @@
+
+const Layout = (props) => {
+  const { children } = props;
+
+  return (
+    <div
+        style={{
+            padding: "0 4em",
+            margin: "auto",
+        }}
+    >
+        {children}
+    </div>
+  );
+};
+
+export default Layout;
