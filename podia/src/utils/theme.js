7d08c8e7355f62e58238331e35301226f39b58c6
HamzaAcb <hamzaachoubir98@gmail.com>
2024-09-08 13:33:51 -0400

    Adding color pallette

---
+++ podia/src/utils/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+    offWhite : '#fdfbf9',
+    white : '#ffffff',
+    darkPurple : '#4b2aad',
+    lightPurple : '#9484c6',
+    violet : '#201445',
+    beige : '#f5efeb',
+}
