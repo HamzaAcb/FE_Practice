f3cadf64687266c561ce115ce18f774eba5ab35d
HamzaAcb <hamzaachoubir98@gmail.com>
2024-09-09 08:32:15 -0400

    Adding grey color

--- podia/src/utils/theme.js
+++ podia/src/utils/theme.js
@@ -1,8 +1,13 @@
-export const theme = {
+const theme = {
+    colors: {
     offWhite : '#fdfbf9',
     white : '#ffffff',
     darkPurple : '#4b2aad',
     lightPurple : '#9484c6',
     violet : '#201445',
     beige : '#f5efeb',
-}
+    darkGrey: "#333333",
+    },
+};
+
+export default theme;
