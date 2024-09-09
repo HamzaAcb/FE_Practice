1539d862cd8d18356945ab81a394c842e1328e26
HamzaAcb <hamzaachoubir98@gmail.com>
2024-09-09 08:37:11 -0400

    Making the initial Navbar

---
+++ podia/src/components/pages/DigitalProducts.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PageLayout from "../common/PageLayout";
+import Nav from "../common/Nav";
+
+
+const DigitalProducts = () => {
+  return (
+    <PageLayout>
+      <Nav/>
+    </PageLayout>
+  );
+};
+
+export default DigitalProducts;
